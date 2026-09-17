b5defc65fcfba27e7677bf7a9f359c552966ef87
rodsth-poc <rodsth@gmail.com>
2026-09-17 05:06:53 -0400

    Add isolated auth POC page

---
+++ app/auth-poc/page.js
@@ -0,0 +1,8 @@
+export default function AuthPocPage() {
+  return (
+    <main>
+      <h1>CBC PocketBase Auth POC</h1>
+      <p>Authentication testing will be performed here.</p>
+    </main>
+  );
+}
